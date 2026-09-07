fd69d71242598eba8db1109ee57a87db27f2e92b
Marcus-rodrigues <74578319+marcus-moneyball@users.noreply.github.com>
2026-09-07 08:10:10 -0300

    Delete api/cron directory

--- api/cron/relatorio-basquete.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { criarHandlerPorEsporte } = require('../../src/cronHandlerPorEsporte');
-
-/**
- * GET /api/cron/relatorio-basquete
- * Protegido por CRON_SECRET (ver vercel.json).
- */
-module.exports = criarHandlerPorEsporte(['basquete'], 'Basquete');
--- api/cron/relatorio-futebol.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-const { criarHandlerPorEsporte } = require('../../src/cronHandlerPorEsporte');
-
-/**
- * GET /api/cron/relatorio-futebol
- * Protegido por CRON_SECRET (ver vercel.json).
- */
-module.exports = criarHandlerPorEsporte(['futebol'], 'Futebol');
